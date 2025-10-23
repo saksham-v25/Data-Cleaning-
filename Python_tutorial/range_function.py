ea835bdaae95e1f63f42de85e3e66cc55dd184be
saksham-v25 <vishwakarmasaksham07@gmail.com>
2025-10-23 21:22:49 +0530

    Add files via upload

---
+++ Python_tutorial/range_function.py
@@ -0,0 +1,19 @@
+# a=range(3,8)
+# for i in a :
+#     print(i)
+
+# a=range(2,8,3)
+# for i in a :
+#     print(i)
+
+
+# a=range(10,0,-1)
+# for i in a :
+#     print(i)
+
+a=range(0,101,1)
+total = 0
+for i in a :
+    total=total+i
+
+print(total)
