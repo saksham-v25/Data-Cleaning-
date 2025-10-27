10d93d0c6e7d4a4440ac099f1e7a1d92f2ccac6b
saksham-v25 <vishwakarmasaksham07@gmail.com>
2025-10-27 19:46:37 +0530

    Add files via upload

---
+++ Python_tutorial/graphic.py
@@ -0,0 +1,7 @@
+from tkinter import Tk
+win=Tk()
+win.title("-----saksham-----")
+win.attributes('-alpha',1)
+win.iconbitmap(r"C:\Users\SAKSHAM\Pictures\coolege project\degree.ico")
+win['bg']= "blue"
+win.mainloop()
