feb0a5fc790890a047027cd823d02a9698cb44e6
saksham-v25 <vishwakarmasaksham07@gmail.com>
2025-11-01 20:11:54 +0530

    Add files via upload

---
+++ Python_tutorial/count.py
@@ -0,0 +1,8 @@
+name="SAKSHAM"
+name_2="vishwakarma"
+string_combine= name + name_2
+#print(string_combine)
+#lower_string=string_combine.lower()
+#print(lower_string)
+a=string_combine.count("a")
+print(a)
