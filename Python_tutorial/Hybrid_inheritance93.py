26a1f4f45c65817e2ec1413ceb602583b1cdf619
saksham-v25 <vishwakarmasaksham07@gmail.com>
2026-01-29 21:16:39 +0530

    Add files via upload

---
+++ Python_tutorial/Hybrid_inheritance93.py
@@ -0,0 +1,17 @@
+class A():
+    def display(self):
+        print("Display from A class")
+
+class B(A):
+    def display(self):
+        print("Display from B class")
+class C():
+    def display(self):
+        print("Hi from C class")
+class D(B,C):
+    def display(self):
+        print("Hi Display from D class")
+
+d1=D()
+d1.display()
+print(D.mro())
